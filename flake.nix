cadfad22eaff44738c8571c300d948705460b590
Tokuhiro Matsuno <tokuhirom@gmail.com>
2026-01-26 13:27:24 +0900

    fix: remove legacyPackages to avoid devbox ambiguity

--- flake.nix
+++ flake.nix
@@ -4,11 +4,13 @@
   outputs = { self, nixpkgs }:
     let
       forAllSystems = nixpkgs.lib.genAttrs nixpkgs.lib.systems.flakeExposed;
+      importPkgs = system: import ./default.nix {
+        pkgs = import nixpkgs { inherit system; };
+      };
     in
     {
-      legacyPackages = forAllSystems (system: import ./default.nix {
-        pkgs = import nixpkgs { inherit system; };
-      });
-      packages = forAllSystems (system: nixpkgs.lib.filterAttrs (_: v: nixpkgs.lib.isDerivation v) self.legacyPackages.${system});
+      packages = forAllSystems (system:
+        nixpkgs.lib.filterAttrs (_: v: nixpkgs.lib.isDerivation v) (importPkgs system)
+      );
     };
 }
